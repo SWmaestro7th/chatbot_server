abe4cd6a397e8ef94f023b54e1776d63315225b3
Hyeonseok Oh <hyeonseok.ohh@gmail.com>
2016-11-24 03:21:27 +0900

    normalize es value

--- paramExtr.py
+++ paramExtr.py
@@ -201,7 +201,8 @@ class ParamExtr(object):
         res = []
         for each in  d['hits']['hits']:
             res.append((each['_source']['name'], each['_score']))
-        return res
+        maxv = res[0][1]
+        return [(x[0], x[1]/maxv) for x in res]
 
     def extrParam(self, cat, ques):
         """
